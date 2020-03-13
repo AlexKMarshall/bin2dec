50ce5bc430fd2f71317340887981665d77d79d6a
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-13 12:30:13 +0000

    Block non-binary input

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const BINARY_REGEX = /^[01]*$/;
+
+const validateBinary = (binary) => BINARY_REGEX.test(binary);
 const binaryToDecimal = (binary) => parseInt(binary, 2);
 
 const binaryInput = document.getElementById('binary-input');
 const decimalResult = document.getElementById('decimal-result');
 
+
 binaryInput.addEventListener('input', (event) => {
+  const binary = binaryInput.value;
+  if (!validateBinary(binary)) {
+    console.log("not valid");
+    decimalResult.innerText = null;
+  }
   decimalResult.innerText = binaryToDecimal(binaryInput.value);
 })
